918d430254d73578572b1292788a11c19b943634
Razvan Bustiuc <bustiucr@gmail.com>
2023-04-23 20:29:46 +0300

    First test

--- src/main/SauceLabsSerenity/features/BaseTest.java
+++ src/main/SauceLabsSerenity/features/BaseTest.java
@@ -2,12 +2,25 @@ package features;
 
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Managed;
-import net.thucydides.core.annotations.Steps;
-import org.fasttrrack.pages.AdminPage;
-import org.fasttrrack.steps.serenity.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
+
+@RunWith(SerenityRunner.class)
 public class BaseTest {
+
+    @Managed(uniqueSession = true)
+    public WebDriver webDriver;
+
+    @Before
+    public void maximizeWindow(){
+        webDriver.manage().window().maximize();
+    }
+
+    @After
+    public void closeDriver(){
+        webDriver.close();
+        webDriver.quit();
+    }
 }
--- src/main/SauceLabsSerenity/features/LoginPageTest.java
+++ src/main/SauceLabsSerenity/features/LoginPageTest.java
@@ -1,5 +1,20 @@
 package features;
-import org.
+import Pages.InventoryPage;
+import Pages.LoginPage;
+import org.junit.Test;
 
-public class LoginPageTest {
+public class LoginPageTest extends BaseTest{
+
+    private LoginPage loginPage;
+    private InventoryPage inventoryPage;
+
+    @Test
+    public void verifyLoginPage(){
+        loginPage.open();
+        loginPage.insertStandardUser("standard_user");
+        loginPage.insertPassword("secret_sauce");
+        loginPage.clickLogin();
+        inventoryPage.verifyPageTile();
+    }
+
 }
